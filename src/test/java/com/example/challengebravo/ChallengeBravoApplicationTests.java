2199b8b19c5e933e7f6ef946ff04d836da506f02
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2023-12-19 14:44:12 -0300

    feat: start application

---
+++ src/test/java/com/example/challengebravo/ChallengeBravoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengebravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBravoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
